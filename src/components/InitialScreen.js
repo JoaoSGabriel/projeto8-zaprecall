3a9853bb0848c93cdbc8696bd849239dcd6c3aec
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-07-19 10:20:27 -0300

    initial Screen complete

--- src/components/InitialScreen.js
+++ src/components/InitialScreen.js
@@ -1,1 +1,9 @@
-alert ('hello world');
+export default function InitialScreen () {
+    return (
+        <div className="initial-Screen">
+            <img src="./img/logo.png" alt="logo"/>
+            <div>ZapRecall</div>
+            <button>Iniciar Recall!</button>
+        </div>
+    );
+}
