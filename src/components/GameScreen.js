53c6a02966c6294a5f43f4025599265b3ea3a56b
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-07-19 14:55:41 -0300

    question structure complete

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -6,22 +6,30 @@ export default function GameScrenn (){
             <p>ZapRecall</p>
         </div>
         <div className="deck-Questions">
-            <div className="question">
-                <p>Pergunta 1</p>
-                <ion-icon name="play-outline"></ion-icon>
+            <div>
+                <div className="question">
+                    <p>Pergunta 1</p>
+                    <ion-icon name="play-outline"></ion-icon>
+                </div>
+                <div className="open-Question">
+                    <div className="front-Face  hidden">
+                        <p>O que é JSX?</p>
+                        <img src="./img/setinha.png" alt="answerView" />
+                    </div>
+                    <div className="back-Face">
+                        <p>JSX é uma sintaxe para escrever HTML dentro do JS</p>
+                        <div>
+                            <button>Não<br/>lembrei</button>
+                            <button>Quase não lembrei</button>
+                            <button>Zap!</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="question">
-                <p>Pergunta 2</p>
-                <ion-icon name="play-outline"></ion-icon>
-            </div>
-            <div className="question">
-                <p>Pergunta 3</p>
-                <ion-icon name="play-outline"></ion-icon>
-            </div>
-            <div className="question">
-                <p>Pergunta 4</p>
-                <ion-icon name="play-outline"></ion-icon>
-            </div>
+        </div>
+        <div className="baseboard">
+            <div className="questions-Complete">0/4 CONCLUÍDOS</div>
+            <div className="questions-icons"></div>
         </div>
     </div>);
 }
