5d5a037e9a62acc457f5d5e5f442f271c06c0ab6
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-07-22 13:44:58 -0300

    game is totally functional

--- src/components/FlashCard.js
+++ src/components/FlashCard.js
@@ -5,6 +5,11 @@ export default function FlashCard (props) {
 
     const [flash_Question, setFlash_Question] = React.useState('question');
     const [flash_Recall, setFLash_Recall] = React.useState('open-Question hidden');
+    
+    function viewQuestion () {
+        setFlash_Question('question hidden');
+        setFLash_Recall('open-Question');
+    }
 
     const [back_Screen, setBack_Screen] = React.useState('back-Face');
     const [front_Screen, setFront_Screen] = React.useState('front-Face');
@@ -14,50 +19,30 @@ export default function FlashCard (props) {
         setFront_Screen('front-Face rotate-Front');
     }
 
-    const [flash_Question_Structure, setFlash_Question_Structure] = React.useState(
-        <div onClick={viewQuestion} className={flash_Question}>
-            <p>Pergunta {number}</p>
-            <ion-icon name="play-outline"></ion-icon>
-        </div>
-    );
-    
-    function viewQuestion () {
-        setFlash_Question('question hidden');
-        setFLash_Recall('open-Question');
-    }
-
+    const [question_Icon, setQuestion_Icon] = React.useState(<ion-icon name="play-outline"></ion-icon>)
     function hideFlashCard (text){
         setFLash_Recall('open-Question hidden');
         if (text === 'red') {
             redRecall();
-            setFlash_Question_Structure(
-                <div className="answered-Question question">
-                    <p style={{color: "#FF3030"}}>Pergunta {number}</p>
-                    <ion-icon style={{color: "#FF3030"}} name="close-circle"></ion-icon>
-                </div>
-            );
+            setFlash_Question('question risk red');
+            setQuestion_Icon(<ion-icon style={{color: "#FF3030"}} name="close-circle"></ion-icon>);
         } else if (text === 'yellow') {
             yellowRecall();
-            setFlash_Question_Structure(
-                <div className="answered-Question question">
-                    <p style={{color: "#FF922E"}}>Pergunta {number}</p>
-                    <ion-icon style={{color: "#FF922E"}} name="help-circle"></ion-icon>
-                </div>
-            );
+            setFlash_Question('question risk yellow');
+            setQuestion_Icon(<ion-icon style={{color: "#FF922E"}} name="help-circle"></ion-icon>);
         } else if (text === 'green'){
             greenRecall();
-            setFlash_Question_Structure(
-                <div className="answered-Question question">
-                    <p style={{color: "#2FBE34"}}>Pergunta {number}</p>
-                    <ion-icon style={{color: "#2FBE34"}} name="checkmark-circle"></ion-icon>
-                </div>
-            );
+            setFlash_Question('question risk green');
+            setQuestion_Icon(<ion-icon style={{color: "#2FBE34"}} name="checkmark-circle"></ion-icon>);
         }
     }
 
     return (
         <>  
-            {flash_Question_Structure}
+            <div onClick={viewQuestion} className={flash_Question}>
+                <p>Pergunta {number}</p>
+                {question_Icon}
+            </div>
             <div className={flash_Recall}>
                 <div className={front_Screen}>
                     <p>{question_Recall}</p>
